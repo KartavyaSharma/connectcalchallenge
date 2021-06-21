79755ad98ba62bd32c4fd8ce36428bbb71d51a5b
KartavyaSharma <Kartavya@outlook.com>
2021-06-21 14:40:58 +0100

    Completed challenge (somewhat)

--- imageUploader/app.py
+++ imageUploader/app.py
@@ -1,12 +1,21 @@
 # Code based on pythonbuddy.com
 
+import os
 from flask import Flask, render_template, request, jsonify, session
+from werkzeug.utils import secure_filename
+import json
 
 # Configure Flask App
 # Remember to change the SECRET_KEY!
+
+UPLOAD_LOCATION = './static/images'
+ACCEPTABLE_EXTENSIONS = {'png', 'jpeg', 'gif', 'jpg'}
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_LOCATION
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 @app.route('/')
 def index():
@@ -14,7 +23,7 @@ def index():
         :return: index.html
     """
     session["count"] = 0
-    return render_template("index.html")
+    return render_template("index.html", image_data={})
 
 '''
 BEGIN TASK 1
@@ -22,11 +31,48 @@ BEGIN TASK 1
 # Should you use a GET or POST Request?
 # @app.route('/submit_image', methods=['POST'])
 # @app.route('/submit_image', methods=['GET'])
+
+def allowed_extensions(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ACCEPTABLE_EXTENSIONS
+
+def store_image(filename):
+    with open('data.json', "r+") as json_file:
+        if os.stat('data.json').st_size == 0:
+            curr_json = {}
+            curr_json["image_uploads"] = []
+            curr_json["image_uploads"].append(filename)
+            json.dump(curr_json, json_file)
+            json_file.close()
+        else:
+            data = json.load(json_file)
+            if filename in data["image_uploads"]:
+                return None
+            data["image_uploads"].append(filename)
+            json_file.close()
+            os.remove(os.getcwd()+"\\data.json")
+            new_json = open('data.json', 'w')
+            json.dump(data, new_json)
+            new_json.close()
+
+
+@app.route('/submit_image', methods=['GET', 'POST'])
 def submit_image():
     """Adds an image to our server and adds it to our webpage. 
         Automatically refreshes our webpage so that user can see the image appended
     """
-    # How do we store images in our server?
+    if request.method == 'POST':
+        f = request.files['image_to_be_uploaded']
+        if f and allowed_extensions(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            store_image(filename)
+        else:
+            return 'File other than an image uploaded!'
+        
+        data = {}
+        with open('data.json', "r+") as json_file:
+            data = json.load(json_file)
+        return render_template('index.html', image_data=data)
 
 '''
 END TASK 1
